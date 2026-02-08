61d8b9087f51f0d31763a1ef8bc6f182a56d6e09
iwan99khairun-del <iwan99khairun@gmail.com>
2026-02-08 12:09:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ if "authenticated" not in st.session_state:
 # --- 2. TAMPILAN HALAMAN LOGIN ---
 if not st.session_state["authenticated"]:
     st.title("🔐 Akses Terbatas")
-    st.markdown("### Alat Konversi LaTeX - by Iwan Gunawan, PhD")
+    st.markdown("### Alat Konversi LaTeX - Docx")
+     st.markdown("### by Iwan Gunawan, PhD")
     st.markdown("### Universiti Mlaysia Al sultan Abdullah")
     input_password = st.text_input("Masukkan Password:", type="password")
     
@@ -94,6 +95,7 @@ if uploaded_file is not None:
 if st.sidebar.button("Keluar / Logout"):
     st.session_state["authenticated"] = False
     st.rerun()
+
 
 
 
