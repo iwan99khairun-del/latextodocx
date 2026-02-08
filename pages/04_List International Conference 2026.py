c7d476b2c8550a30e13e1f71423bc35bd18ea11c
iwan99khairun-del <iwan99khairun@gmail.com>
2026-02-08 18:02:53 +0700

    Create 04_List International Conference 2026.py

---
+++ pages/04_List International Conference 2026.py
@@ -0,0 +1,48 @@
+conferences_2026 = [
+    {
+        "name": "APCOMS-IMEC 2026",
+        "location": "Jakarta, Indonesia",
+        "date": "Late 2026 (TBA)",
+        "url": "https://apcoms-imec.fti.trisakti.ac.id/",
+        "indexing": "Scopus (via Springer LNME)"
+    },
+    {
+        "name": "14th International Conference on Mechanical and Manufacturing Engineering (ICME 2026)",
+        "location": "Johor, Malaysia (UTHM)",
+        "date": "August 5 - 6, 2026",
+        "url": "https://conference.uthm.edu.my/index.php/icme/index",
+        "indexing": "Scopus (Accepted papers)"
+    },
+    {
+        "name": "ASME International Mechanical Engineering Congress & Exposition (IMECE 2026)",
+        "location": "Vancouver, Canada",
+        "date": "November 8 - 12, 2026",
+        "url": "https://www.asme.org/conferences-events/events/imece",
+        "indexing": "Scopus / WoS"
+    },
+    {
+        "name": "ASME Turbomachinery Technical Conference & Exposition (Turbo Expo 2026)",
+        "location": "Milan, Italy",
+        "date": "June 15 - 19, 2026",
+        "url": "https://event.asme.org/Turbo-Expo",
+        "indexing": "Scopus / WoS"
+    },
+    {
+        "name": "17th IEEE International Conference on Mechanical and Aerospace Engineering (ICMAE 2026)",
+        "location": "London, UK",
+        "date": "July 15 - 18, 2026",
+        "url": "https://icmae.org/",
+        "indexing": "Scopus / IEEE Xplore"
+    },
+    {
+        "name": "13th International Conference on Mechanical, Automotive and Materials Engineering (CMAME 2026)",
+        "location": "Singapore",
+        "date": "December 18 - 20, 2026",
+        "url": "http://www.cmame.net/",
+        "indexing": "Scopus / Ei Compendex"
+    }
+]
+
+# Quick check: List all URLs
+for conf in conferences_2026:
+    print(f"{conf['name']}: {conf['url']}")
